1785423bf0c58b958982c7119757eba9bfb75132
Zahy Bnaya <zb9@login-0-1.local>
2015-01-30 13:35:45 -0500

    logging the mextfunction

--- src/mexfunction.cpp
+++ src/mexfunction.cpp
@@ -3,17 +3,14 @@
 #include <string>
 	
 void mexFunction(int nlhs, mxArray *plhs[],int nrhs, const mxArray *prhs[]){
-	//heuristic h;
+	FILELog::ReportingLevel() = FILELog::FromString("DEBUG");
+	FILE_LOG(logDEBUG) << " Starting mexFunction" << std::endl;
 	data_struct dat;
 	unsigned int seed=unsigned(time(0));
-	mt19937_64 global_generator;
 	double* playerptr=mxGetPr(prhs[0]);
 	int player=((int) (*playerptr+0.5)); 
 	char* agent_file =mxArrayToString(prhs[1]);
 	double* paramptr=mxGetPr(prhs[2]);
-	global_generator.seed(seed);
-	h.seed_generator(global_generator);
-	//h.get_params_from_matlab(paramptr);
 	Agent_params ap = read_agent_params(std::string(agent_file));
 	FILE_LOG(logDEBUG) << " Starting concrete process for "<<agent_file << std::endl;
 	concrete(ap,paramptr);
