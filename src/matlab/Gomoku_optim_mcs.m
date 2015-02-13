cb958064c4a73c124560345c019494153db82322
Zahy Bnaya <zahy.bnaya@gmail.com>
2015-02-12 22:23:32 -0500

    adding data_file to mex

--- src/matlab/Gomoku_optim_mcs.m
+++ src/matlab/Gomoku_optim_mcs.m
@@ -1,4 +1,4 @@
-function Gomoku_optim_mcs(player,agent_file)
+function Gomoku_optim_mcs(player,agent_file,data_file)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% runmcs.m %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -44,7 +44,7 @@ addpath(strcat(home,'/mnk/src/matlab/mcs/gls'))
 
 
 fcn = 'Gomoku_wrapper';
-data = {player, agent_file};
+data = {player, agent_file, data_file};
 % define bounds on variables (+-inf allowed)
 %
 % u: column vector of lower bounds
--- src/mexfunction.cpp
+++ src/mexfunction.cpp
@@ -6,12 +6,14 @@
 void mexFunction(int nlhs, mxArray *plhs[],int nrhs, const mxArray *prhs[]){
 	FILELog::ReportingLevel() = FILELog::FromString("ERROR");
 	FILE_LOG(logDEBUG) << " Starting mexFunction" << std::endl;
-	data_struct dat;
 	unsigned int seed=unsigned(time(0));
 	double* playerptr=mxGetPr(prhs[0]);
 	int player=((int) (*playerptr+0.5)); 
 	char* agent_file =mxArrayToString(prhs[1]);
-	double* paramptr=mxGetPr(prhs[2]);
+	char* data_file =mxArrayToString(prhs[2]);
+	data_struct dat;
+       	dat = load_data(dat,std::string(data_file));
+	double* paramptr=mxGetPr(prhs[3]);
 	Agent_params ap = read_agent_params(std::string(agent_file));
 	FILE_LOG(logDEBUG) << " Starting concrete process for "<<agent_file << std::endl;
 	concrete(ap,paramptr);
