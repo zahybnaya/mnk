9b32b8907db36098c1292faa9095a2da9fdd8bca
Zahy Bnaya <zb9@login-0-1.local>
2015-04-24 12:19:43 -0400

    comments on script and readme

--- src/matlab/cross_validate.m
+++ src/matlab/cross_validate.m
@@ -5,18 +5,20 @@ function cross_validate(player,agent_file,train_data_file, test_data_file)
 	if exist(filename,'file')
 		load(filename)
 		L=zeros(nbasket,30);
+		% training 
 		for j=1:30
 			for i=1:nbasket
 				L(i,j)=Gomoku_wrapper(data,xmin(:,i));
 			end
 		end
 		[l,i]=min(mean(L,2))
-		x=xmin(:,i)
+		x=xmin(:,i) %best values are stored in x
 		L
 		result_file_name = sprintf('optimization_results%d.mat',player)
 		save(result_file_name,'L','x','l','i','-mat')
 		data = {player, agent_file, test_data_file};
 		LT=zeros(1,30)
+		% testing
 		for t=1:30
 			LT(t)=Gomoku_wrapper(data,x);
 		end
