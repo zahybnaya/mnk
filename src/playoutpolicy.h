684497e03db872aebb5294b595157db3264bce48
Zahy Bnaya <zahy.bnaya@gmail.com>
2015-09-20 18:07:52 +0300

    added missing file

---
+++ src/playoutpolicy.h
@@ -0,0 +1,14 @@
+#ifndef PLAYOUTPOLICY_H
+#define PLAYOUTPOLICY_H
+#include "board.h"
+
+class PlayoutPolicy {
+	public:
+		PlayoutPolicy(){};
+		virtual ~PlayoutPolicy(){};
+		virtual double eval(board b) = 0;
+
+};
+
+
+#endif /* end of include guard: PLAYOUTPOLICY_H */
